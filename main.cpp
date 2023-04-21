00f7bbecdf76f1501ed30da7bd65f6a45fb4713a
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-04-21 11:46:30 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <Novice.h>
+#include "Vector3D/Vector3D.h"
+#include "Mat4x4/Mat4x4.h"
+
+const char kWindowTitle[] = "LE2A_04_キクタニ_タクマ_タイトル";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	Vector3D scale(1.2f, 0.79f, -2.1f);
+	Vector3D rotate(0.4f, 1.43f, -0.8f);
+	Vector3D translate(2.7f, -4.15f, 1.57f);
+	Mat4x4 worldMatrix = MakeMatrixAffin(scale, rotate, translate);
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
